6b2a509714bb6c6f79eeb171c7d2759350d3d4c8
Adam Chisolm <axchisolm@gmail.com>
2019-01-22 09:59:59 -0600

    Simple input/output

--- main.c
+++ main.c
@@ -9,6 +9,14 @@
 
 int main(int argc, char* argv[])
 {
+    int MAX_INPUT = 100;
+    char user_input [MAX_INPUT];
+
+    while (1) {
+        printf("prompt> ");
+        fgets(user_input, MAX_INPUT, stdin);
+        printf("%s", user_input);
+    }
     printf("hello\n");
 
     return 0;
