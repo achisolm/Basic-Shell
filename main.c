f60b2706770a874b176f93551c357d6b7106a7ad
Adam Chisolm <axchisolm@gmail.com>
2019-01-22 09:47:29 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * Authors: Adam Chisolm and Jon Sulcer
+ * Date: 5 Feb. 2019
+ * File: main.c
+ * Description: A basic implementation of the Unix shell.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("hello\n");
+
+    return 0;
+}
