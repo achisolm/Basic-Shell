4a18a4b88c83923c12c91fd8ce1a6531b63c41f8
Jon Sulcer <jonasulc@ut.utm.edu>
2019-02-05 19:07:37 -0600

    Update shell.c

--- shell.c
+++ shell.c
@@ -8,58 +8,97 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
+const int MAX_INPUT = 100;
+
+void interactive();
+void batch(char* file_name);
 void parse_command(char* str, char** parsed_command);
 int parse_line(char* line, char** parsed_line);
+void execute_line(char* line);
 
 int main(int argc, char* argv[])
 {
-    int MAX_INPUT = 100;
+    // Interactive Mode
+    if (argc == 1) {
+        interactive();
+    }
+
+    // Batch Mode
+    else if (argc == 2) {
+        batch(argv[1]);
+    }
+
+    // Error Mode
+    else {
+        printf("ERROR: Wrong number of arguments\n");
+    }
+    
+    return 0;
+}
+
+void interactive() {
     char user_input [MAX_INPUT];
-    char* parsed_command [MAX_INPUT];
-    char* parsed_line [MAX_INPUT];
 
     while (1) {
         printf("prompt> ");
         fgets(user_input, MAX_INPUT, stdin);
-        strtok(user_input, "\n");
-        /*
-        if (strcmp(user_input, "exit") == 0) {
-            break;
-        }
-        */
-
-
-        int num_commands = parse_line(user_input, parsed_line);
-
-        for (int i = 0; i < num_commands; i++) {
-            parse_command(parsed_line[i], parsed_command);
-
-            if (strcmp(parsed_command[0], "exit") == 0) {
-                exit(0);
-            }
-
-            pid_t pid;
-            int status;
-
-            pid = fork();
-
-            if (pid < 0) {
-                printf("ERROR: Unable to execute!");
-            }
-            else if (pid == 0) {
-                execvp(parsed_command[0], parsed_command);
-            }
-            else {
-                while (wait(&status) != pid);
-            }
-        }
         
+        execute_line(user_input);
+    }
+}
+
+void batch(char* file_name) {
+    
+    char line[MAX_INPUT];
+    FILE *batch_file;
+    batch_file = fopen(file_name, "r");
+
+    if (batch_file == NULL) {
+        printf("ERROR: Could not open file\n");
+        exit(0);
     }
 
-    
+    while (fgets(line, MAX_INPUT, batch_file)) {
+        execute_line(line);
+    }
 
-    return 0;
+    fclose(batch_file);
+}
+
+void execute_line(char* line) {
+    
+    char* parsed_command [MAX_INPUT];
+    char* parsed_line [MAX_INPUT];
+
+    strtok(line, "\n");
+
+    int num_commands = parse_line(line, parsed_line);
+
+    for (int i = 0; i < num_commands; i++) {
+        parse_command(parsed_line[i], parsed_command);
+
+        pid_t pid;
+        int status;
+
+        if (strcmp(parsed_command[0], "exit") == 0) {
+            exit(0);
+        }
+
+        pid = fork();
+
+        if (pid < 0) {
+            printf("ERROR: Unable to execute!");
+        }
+        else if (pid == 0) {
+            execvp(parsed_command[0], parsed_command);
+        }
+
+        else {
+            while (wait(&status) != pid);
+        }
+    }
 }
 
 void parse_command(char* str, char** parsed_command) {
